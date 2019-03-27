d2b9fae91fd453002599f21b97c6c40a25e9e40f
Tony Smith <tonysmithbupt@gmail.com>
2019-03-27 11:27:13 +0800

    新闻分类

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,27 @@
 //index.js
-Page({})
+Page({
+    data: {
+        tabList: [{
+                name: '国内'
+            },
+            {
+                name: '国际'
+            },
+            {
+                name: '财经'
+            },
+            {
+                name: '娱乐'
+            },
+            {
+                name: '军事'
+            },
+            {
+                name: '体育'
+            },
+            {
+                name: '其他'
+            },
+        ],
+    },
+})
